1bff7c307a86f32bab2ff006c8c54718b6e1354d
agungandre687 <agungandre687@gmail.com>
2024-07-25 11:15:37 +0000

    migrations

---
+++ prisma/migrations/20240725105547_autoincrement/migration.sql
@@ -0,0 +1,32 @@
+-- AlterTable
+ALTER TABLE `master_agama` MODIFY `id` INTEGER NOT NULL AUTO_INCREMENT;
+
+-- AlterTable
+ALTER TABLE `master_cacat` MODIFY `id` INTEGER NOT NULL AUTO_INCREMENT;
+
+-- AlterTable
+ALTER TABLE `master_golongan_darah` MODIFY `id` INTEGER NOT NULL AUTO_INCREMENT;
+
+-- AlterTable
+ALTER TABLE `master_pekerjaan` MODIFY `id` INTEGER NOT NULL AUTO_INCREMENT;
+
+-- AlterTable
+ALTER TABLE `master_pendidikan` MODIFY `id` INTEGER NOT NULL AUTO_INCREMENT;
+
+-- AlterTable
+ALTER TABLE `master_penduduk_hubungan` MODIFY `id` INTEGER NOT NULL AUTO_INCREMENT;
+
+-- AlterTable
+ALTER TABLE `master_penduduk_status_dasar` MODIFY `id` INTEGER NOT NULL AUTO_INCREMENT;
+
+-- AlterTable
+ALTER TABLE `master_penduduk_status_kawin` MODIFY `id` INTEGER NOT NULL AUTO_INCREMENT;
+
+-- AlterTable
+ALTER TABLE `master_sakit_menahun` MODIFY `id` INTEGER NOT NULL AUTO_INCREMENT;
+
+-- AlterTable
+ALTER TABLE `master_status` MODIFY `id` INTEGER NOT NULL AUTO_INCREMENT;
+
+-- AlterTable
+ALTER TABLE `master_suku` MODIFY `id` INTEGER NOT NULL AUTO_INCREMENT;
