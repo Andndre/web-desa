bf90b8b8c6682c5aca8d3a12e4e980223e81a570
agungandre687 <agungandre687@gmail.com>
2024-07-25 23:06:06 +0000

    template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+import ECommerce from "@/components/Dashboard/E-commerce";
+import { Metadata } from "next";
+import DefaultLayout from "@/components/Layouts/DefaultLayout";
+
+export const metadata: Metadata = {
+  title:
+    "Next.js E-commerce Dashboard | TailAdmin - Next.js Dashboard Template",
+  description: "This is Next.js Home for TailAdmin Dashboard Template",
+};
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello World
-    </main>
+    <>
+      <DefaultLayout>
+        <ECommerce />
+      </DefaultLayout>
+    </>
   );
 }
