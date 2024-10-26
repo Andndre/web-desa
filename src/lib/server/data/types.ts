80b97a59859c58502f0207693f9b6abf67bde0e3
andndre <agungandre687@gmail.com>
2024-10-27 07:52:34 +0800

    remove unused types

--- src/lib/server/data/types.ts
+++ src/lib/server/data/types.ts
@@ -1,12 +1,3 @@
-import * as pendudukData from "./pendudukData";
-import * as kartuKeluargaData from "./kartuKeluargaData";
-
-export type PendudukData = Awaited<ReturnType<typeof pendudukData.getDataPenduduk>>;
-export type PendudukDataRow = PendudukData[number];
-
-export type KeluargaData = Awaited<ReturnType<typeof kartuKeluargaData.getDataKeluarga>>;
-export type KeluargaDataRow = KeluargaData[number];
-
 export type ReturnTypeItemAsync<
   T extends (page: number, perPage: number, ...args: any[]) => Promise<any[]>
 > = Awaited<ReturnType<T>>[number];
